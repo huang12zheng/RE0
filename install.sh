63882f0782bf33ca9456ebba4513e374994389f2
huang12zheng <huang12zheng@163.com>
2021-07-01 21:47:43 +0800

    rm old

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-# ansible-galaxy install gantsign.git_user
-# ansible-galaxy install gantsign.visual-studio-code
-# ansible-galaxy install gantsign.oh-my-zsh
+ansible-galaxy install gantsign.git_user
+ansible-galaxy install gantsign.visual-studio-code
+ansible-galaxy install gantsign.oh-my-zsh
 
 # ansible-playbook -i hosts clash_install.yaml &
 # ansible-playbook -i hosts docker_install.yaml &
