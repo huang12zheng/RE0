8a60c38a7e8c07e7efd2a6b5242bee59d2829c89
huang12zheng <805104533@qq.com>
2021-07-10 17:00:42 +0800

    fix bug create netplan

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_pr
 ansible-galaxy install gantsign.git_user
 ansible-galaxy install gantsign.visual-studio-code
 ansible-galaxy install gantsign.oh-my-zsh
+# ansible-galaxy install ansible-netplan,v0.2.0
+ansible-galaxy install mrlesmithjr.netplan
 
 # ansible-playbook -i hosts clash_install.yaml &
 # ansible-playbook -i hosts docker_install.yaml &
